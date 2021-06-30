2ba37e0bb17ee07b7464d09fd4915555ce8e7a9b
iftesar <71936912+iftesar@users.noreply.github.com>
2021-06-30 22:13:11 +0530

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,7 +1,7 @@
 // Listen for Submit
 document.getElementById('submit').addEventListener('submit', function(e){
  // Hide Results
- document.getElementById('results').style.display = 'none'
+ //document.getElementById('results').style.display = 'none'
  document.getElementById('submit').style.display = "none"
 
  // Show Loading
@@ -14,13 +14,13 @@ document.getElementById('submit').addEventListener('submit', function(e){
 // display Results
 function display() {
  document.getElementById('loading').style.display = 'none'
- document.getElementById('results').style.display = 'block'
- var node = document.createElement("p");                 
- var textnode = document.createTextNode({{ prediction_text }});         
- node.appendChild(textnode);                              
- document.getElementById("results").appendChild(node);     
- document.querySelector(".input").readOnly = true
  document.getElementById('submit').style.display = "block"
+ document.getElementById('results').style.display = 'block'
+ //var node = document.createElement("p");                 
+ //var textnode = document.createTextNode({{ prediction_text }});         
+ //node.appendChild(textnode);                              
+ //document.getElementById("results").appendChild(node);     
+ document.querySelector(".input").readOnly = true
  document.querySelector('.submit').style.display = 'none'
  document.querySelector('.title').innerHTML = 'Results'
  document.querySelector('.subtitle').style.display = 'none'
